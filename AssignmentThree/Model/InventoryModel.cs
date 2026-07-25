7159f6b8704e6dd7194e1786f8a0dd1cc4767917
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-07-25 11:50:44 +0530

    build : mcv folder structure

---
+++ AssignmentThree/Model/InventoryModel.cs
@@ -0,0 +1,40 @@
+﻿namespace AssignmentThree.Model
+{
+    /// <summary>
+    /// Model for the Inventory
+    /// </summary>
+    public class InventoryModel
+    {
+        /// <summary>
+        /// Gets or Sets the Id of the product
+        /// </summary>
+        /// <value>
+        /// A string containing the Id of the Product.
+        /// </value>
+        public int Id { get; set; }
+
+        /// <summary>
+        ///  Gets or sets the Name of the Product
+        /// </summary>
+        /// <value>
+        /// A string containing the Name of the Product.
+        /// </value>
+        public string? Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Product Price.
+        /// </summary>
+        /// <value>
+        /// Conatins the Price of the product.
+        /// </value>
+        public long Price { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Product Quantity.
+        /// </summary>
+        /// <value>
+        /// Conatins the Quantity of the product.
+        /// </value>
+        public long Quantity { get; set; }
+    }
+}
---
+++ AssignmentThree/Repository/InventoryRepository.cs
@@ -0,0 +1,15 @@
+﻿using AssignmentThree.Model;
+
+namespace AssignmentThree.Repository
+{
+    /// <summary>
+    /// Its used to store and retrive data (In-Memory)
+    /// </summary>
+    public class InventoryRepository
+    {
+        /// <summary>
+        /// List to store the Inventory 
+        /// </summary>
+        private List<InventoryModel> _inventory = new List<InventoryModel>();
+    }
+}
