d7edfd5fbd5305d2ea4a89450dfd8cdfbe2aa295
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-07-31 16:44:16 +0530

    build:created model

--- src/AssignmentFour/Program.cs
+++ src/AssignmentFour/Program.cs
@@ -4,23 +4,11 @@ using AssignmentFour.View;
 
 namespace Assignments
 {
-    /// <summary>
-    /// To start the application
-    /// </summary>
-    public class Program
+    internal class Program
     {
-        /// <summary>
-        /// Entry point for the Application.
-        /// </summary>
-        /// <param name="args">Default arguments</param>
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            IRepository repository = new TransactionRepository();
-            TransactionService service = new TransactionService(repository);
-            InputView inputView = new InputView(service);
-            TransactionView view = new TransactionView(service, inputView);
-
-            view.StartApplication();
+            Console.WriteLine("Hello, World!");
         }
     }
 }
