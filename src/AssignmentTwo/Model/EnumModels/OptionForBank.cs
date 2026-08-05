5f23a2517365924cc5fb7baf8f6700d82d48a468
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-08-05 09:06:05 +0530

    Apply suggestion from @Preethxsuresh
    Co-authored-by: Preethi Sureshkumar <141893681+Preethxsuresh@users.noreply.github.com>

--- src/AssignmentTwo/Model/EnumModels/OptionForBank.cs
+++ src/AssignmentTwo/Model/EnumModels/OptionForBank.cs
@@ -3,7 +3,7 @@
     /// <summary>
     /// Options for the Bank
     /// </summary>
-    public enum OptionForBank
+    public enum BankOptions
     {
         /// <summary>
         /// Option to create and view Savings account.
