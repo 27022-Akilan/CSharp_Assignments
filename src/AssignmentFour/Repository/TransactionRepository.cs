cd2b2e366f2bc6dc661f27c1ba0ef108877d441e
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-08-07 11:50:15 +0530

    fix : Irepository

--- src/AssignmentFour/Repository/TransactionRepository.cs
+++ src/AssignmentFour/Repository/TransactionRepository.cs
@@ -1,5 +1,5 @@
-﻿using AssignmentFour.Constants;
-using AssignmentFour.Model;
+﻿using AssignmentFour.Model;
+
 namespace AssignmentFour.Repository
 {
     /// <summary>
@@ -36,26 +36,6 @@ namespace AssignmentFour.Repository
                     field.Amount = transaction.Amount;
                     field.Date = transaction.Date;
                     field.Description = transaction.Description;
-                    field.Category = transaction.Category;
-                    return true;
-                }
-            }
-
-            return false;
-        }
-
-        /// <summary>
-        /// To delete the Transaction using Id
-        /// </summary>
-        /// <param name="transactionId">Id of the transaction to be deleted</param>
-        /// <returns>True if the transaction is found and deleted | False if the transaction cannot be deleted</returns>
-        public bool DeleteTransactionById(Guid transactionId)
-        {
-            Transaction? transaction = this._transactionList.FirstOrDefault(t => t.TransactionId == transactionId);
-            if (transaction == null)
-            {
-                return false;
-            }
 
                     if (transaction.TransactionType == Model.Type.Income)
                     {
@@ -68,16 +48,6 @@ namespace AssignmentFour.Repository
 
                     return true;
                 }
-
-        /// <summary>
-        /// Shows the transactions of the desired description
-        /// </summary>
-        /// <param name="description">Description of transactions to retrieve</param>
-        /// <returns>IEnumerable list of Transactions of the specified description</returns>
-        public IEnumerable<Transaction> GetTransactionsByDescription(string description)
-        {
-            return this._transactionList.Where(t => t.Description.Contains(description))
-                                        .Select(t => t.CloneTransaction());
             }
 
             return false;
