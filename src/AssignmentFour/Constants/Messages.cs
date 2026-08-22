b6d085ae08962d38b30be6f2db8a9446fa73abc1
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-08-22 11:47:36 +0530

    refactor : View layer

--- src/AssignmentFour/Constants/Messages.cs
+++ src/AssignmentFour/Constants/Messages.cs
@@ -29,5 +29,30 @@
         /// Adding the transaction failed due to invalid type of transaction.
         /// </summary>
         public const string CantAddDueToInvalidType = "Cant add the transaction, due to invalid type of transaction";
+
+        /// <summary>
+        /// Validation of description failed message
+        /// </summary>
+        public const string ValidationErrorOnDescription = "The description can't be Empty or Whitespace";
+
+        /// <summary>
+        /// Type of the Amount is not correct message.
+        /// </summary>
+        public const string InvalidAmountFormat = "Your input Should contains number only!";
+
+        /// <summary>
+        /// Invalid Amount.
+        /// </summary>
+        public const string ValidationErrorOnAmount = "Your amount should be greater than 0";
+
+        /// <summary>
+        /// Invalid Date format message.
+        /// </summary>
+        public const string InvalidDateFormat = "Invalid date format. Please enter as DD/MM/YYYY";
+
+        /// <summary>
+        /// Validation error message for Date.
+        /// </summary>
+        public const string ValidationErrorOnDate = "Invalid date : you input date should be less than or equal to current date.";
     }
 }
