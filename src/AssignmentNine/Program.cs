e38a0ae009e26015d5a771bcaf72c092f723b43c
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-09-02 15:10:05 +0530

    fix : removed comments on program.cs

--- src/AssignmentNine/Program.cs
+++ src/AssignmentNine/Program.cs
@@ -15,41 +15,11 @@ namespace Assignments
         /// <param name="args">Default parameter</param>
         public static void Main(string[] args)
         {
-            //Console.WriteLine("\t\t\t\t\t\t Language Integrated Query\n");
             InventoryCreation inventoryCreation = new InventoryCreation();
             List<Product> products = inventoryCreation.CreateProducts();
             List<Supplier> suppliers = inventoryCreation.CreateSupplier();
             MenuHandler menuHandler = new MenuHandler(products, suppliers);
             menuHandler.Run();
-            //BasicQueries taskOne = new BasicQueries(products);
-            //Console.WriteLine("The initial products are ");
-            //TablePresenter.DisplayProducts(products);
-            //taskOne.PerformTask();
-
-            //ConsoleCleaner.Clean();
-
-            //ComplexQueries taskTwo = new ComplexQueries(products, suppliers);
-            //TablePresenter.DisplayProducts(products);
-            //taskTwo.GroupByCategory();
-
-            //ConsoleCleaner.Clean();
-
-            //taskTwo.RelateProductAndSupplier();
-
-            //ConsoleCleaner.Clean();
-
-            //ObjectQueries taskThree = new ObjectQueries();
-            //taskThree.PerformArrayOperations();
-
-            //ConsoleCleaner.Clean();
-
-            //QueryOptimization taskFour = new QueryOptimization(products);
-            //taskFour.SortBooksByPrice();
-
-            //ConsoleCleaner.Clean();
-
-            //CustomQueries taskFive = new CustomQueries(products, suppliers);
-            //taskFive.WriteQueries();
         }
     }
 }
