15ac9b0a205caa81c09cb8a9348f5af4ab64a21a
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-09-03 17:58:40 +0530

    fix : removed comments

--- src/AssignmentFour/Model/RequestModel/IncomeRequestModel.cs
+++ src/AssignmentFour/Model/RequestModel/IncomeRequestModel.cs
@@ -1,6 +1,6 @@
 ﻿using AssignmentFour.Model.Enums;
 
-namespace AssignmentFour.Model.DTO
+namespace AssignmentFour.Model.RequestModel
 {
     /// <summary>
     /// Represents a request model for the Income.
