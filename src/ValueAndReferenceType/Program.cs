e1feb2423700eba8a308091f150424ea4b063f49
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-09-04 16:21:27 +0530

    build : task 1

---
+++ src/ValueAndReferenceType/Program.cs
@@ -0,0 +1,23 @@
+﻿using ValueAndReferenceType;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Entry point of the application.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Starts the application.
+        /// </summary>
+        /// <param name="args">Default arguments</param>
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            ValueAndReference valueAndReference = new ValueAndReference();
+
+            valueAndReference.CreateValue();
+        }
+    }
+}
---
+++ src/ValueAndReferenceType/ValueAndReference.cs
@@ -0,0 +1,75 @@
+﻿using ValueAndReferenceType.Model;
+
+namespace ValueAndReferenceType
+{
+    /// <summary>
+    /// Represents the task of value and reference type.
+    /// </summary>
+    public class ValueAndReference
+    {
+        /// <summary>
+        /// To create value and reference type.
+        /// </summary>
+        public void CreateValue()
+        {
+            bool isValid = this.TryGetNumber("\nEnter a number to put into value(struct) and reference(class) type : ", out int number);
+            if (!isValid)
+            {
+                return;
+            }
+
+            ValueAndReferenceType.Model.ValueType valueType = new();
+            valueType.Number = number;
+
+            ReferenceType referenceType = new ReferenceType();
+            referenceType.Number = number;
+
+            if (!this.ModifyData(valueType, referenceType))
+            {
+                Console.WriteLine("Invalid Input Cannot change !!");
+                return;
+            }
+
+            Console.WriteLine("============================" +
+                              "\n--- Value Type ---" +
+                              "\n============================");
+            Console.WriteLine($"Before change : {number}" +
+                              $"\nAfter change : {valueType.Number}");
+
+            Console.WriteLine("============================" +
+                              "\n--- Reference Type ---" +
+                              "\n============================");
+            Console.WriteLine($"Before change : {number}" +
+                              $"\nAfter change : {referenceType.Number}");
+        }
+
+        private bool ModifyData(ValueAndReferenceType.Model.ValueType valueTypeData, ReferenceType referenceTypeData)
+        {
+            bool isValid = this.TryGetNumber("\nEnter a number to change the data of the value type and reference type : ", out int number);
+            if (!isValid)
+            {
+                return isValid;
+            }
+
+            Console.WriteLine($"Changing value to {number}");
+            int valueTemp = valueTypeData.Number;
+            valueTypeData.Number = number;
+            int referenceTemp = referenceTypeData.Number;
+            referenceTypeData.Number = number;
+            return isValid;
+        }
+
+        private bool TryGetNumber(string prompt, out int number)
+        {
+            Console.Write(prompt);
+            string input = Console.ReadLine() ?? string.Empty;
+            if (!int.TryParse(input, out number))
+            {
+                Console.WriteLine("Invalid Input !!");
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
