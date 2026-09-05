843d937b7255fa51778a7f1679ae47f89a680a21
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-09-05 13:02:25 +0530

    fix : parameter change in GetTotMem method task 2

--- src/GarbageCollector/Program.cs
+++ src/GarbageCollector/Program.cs
@@ -13,23 +13,21 @@ namespace Assignments
         /// <param name="args">Default arguments</param>
         public static void Main(string[] args)
         {
-            Console.WriteLine($"The initial memory before object creation is : {GC.GetTotalMemory(true)}");
+            Console.WriteLine($"The initial memory before object creation is : {GC.GetTotalMemory(false)}");
             List<Person>? people = new List<Person>();
-            Console.ReadKey();
             for (int i = 0; i < 1000000; i++)
             {
                 people.Add(new Person());
             }
 
-            Console.WriteLine($"Generation of the List : {GC.GetGeneration(people)}");
             Console.ReadKey();
+            Console.WriteLine($"Generation of the List : {GC.GetGeneration(people)}");
             Console.WriteLine($"\nThe memory after object creation is : {GC.GetTotalMemory(false)}");
             people = null;
             Console.WriteLine("\nCollecting the garbage unreferenced objects ....");
             Console.ReadKey();
             GC.Collect();
-            Console.ReadKey();
-            Console.WriteLine($"\nThe memory after object destruction is : {GC.GetTotalMemory(false)}");
+            Console.WriteLine($"\nThe memory after object destruction is : {GC.GetTotalMemory(true)}");
             Console.WriteLine("\nCollected the unreferenced objects.");
         }
     }
