ce9144a4c31505c3fdbae2d74d05018dfa98f105
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-09-07 18:11:09 +0530

    fix : handle exception in main

--- src/AssignmentFour/Program.cs
+++ src/AssignmentFour/Program.cs
@@ -1,4 +1,5 @@
-﻿using AssignmentFour.Repository;
+﻿using AssignmentFour;
+using AssignmentFour.Repository;
 using AssignmentFour.Service;
 using AssignmentFour.View;
 
@@ -10,20 +11,42 @@ namespace Assignments
     public class Program
     {
         /// <summary>
+        /// Path of the file to store the transactions.
+        /// </summary>
+        public const string FilePath = @"Transactions.json";
+
+        /// <summary>
         /// Entry point for the Application.
         /// </summary>
         /// <param name="args">Default arguments</param>
         public static void Main(string[] args)
         {
-            string filePath = @"Transactions.json";
+            try
+            {
+                // IRepository repository = new TransactionRepository();
+                IRepository fileRepository = new FileRepository(FilePath);
+                TransactionService service = new TransactionService(fileRepository);
+                InputView inputView = new InputView(service);
+                TransactionView transactionView = new TransactionView(service, inputView);
 
-            // IRepository repository = new TransactionRepository();
-            IRepository fileRepository = new FileRepository(filePath);
-            TransactionService service = new TransactionService(fileRepository);
-            InputView inputView = new InputView(service);
-            TransactionView transactionView = new TransactionView(service, inputView);
-
-            transactionView.StartApplication();
+                transactionView.StartApplication();
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                Helper.DisplayErrorMessage($"\nError: The application does not have permission to access the file.\n{ex.Message}");
+            }
+            catch (System.IO.IOException ex)
+            {
+                Helper.DisplayErrorMessage($"Error accessing the repository file: {ex.Message}");
+            }
+            catch (System.Text.Json.JsonException ex)
+            {
+                Helper.DisplayErrorMessage($"Error: The existing file {FilePath} is corrupted or not properly formatted.\n{ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Helper.DisplayErrorMessage($"Error : Unexpected Error , Please try after some time,\n{ex.Message}");
+            }
         }
     }
 }
