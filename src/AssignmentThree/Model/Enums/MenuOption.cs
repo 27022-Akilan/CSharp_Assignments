8a95ad7b02e93451e9cf13fdab6cbcbdb69228cd
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-09-10 17:44:31 +0530

    build : Complete Assignment3 (Inventory management)
    * build : mcv folder structure
    
    * build : done with creation of list
    
    * build : created menu
    
    * fix : solution file fixed
    
    * fix : Added else part for invalid id on edit part
    
    * fix : search logic
    
    * fix : corrected color for the print messages on Add
    
    * fix : typos
    
    * fix : search null logic
    
    * fix : displayed products before deleting
    
    * fix : try-catch login for serach
    
    * refactor : sepeared Serach logic view into methods
    
    * fix : build errors on .csproj file
    
    * refactor : edit & serach logic to user friendly
    
    * fix : comments
    
    * fix : space between names
    
    * fix : removed debug lines
    
    * fix : typo on Method name
    
    * fix : typo on comments
    
    * fix : typos
    
    * fix : the logic of injecting by interface
    
    * fix : typos and removed unwanted methods
    
    * fix : typo on sevice
    
    * refactor : splitted reader and presenter
    
    * fix : name of variable byname
    
    * fix : resolved first 2 comments
    
    * fix : comments resolved on rest of the files rather than view layer
    
    * fix : comments on view layer
    
    * fix : changed the App name into display method
    
    * fix : comments
    
    * fix : pr-comment unwanted commemt statement

---
+++ src/AssignmentThree/Model/Enums/MenuOption.cs
@@ -0,0 +1,38 @@
+﻿namespace AssignmentThree.Model.Enums
+{
+    /// <summary>
+    ///  Enums to specify the Main menu option
+    /// </summary>
+    public enum MenuOption
+    {
+        /// <summary>
+        /// To add product
+        /// </summary>
+        AddProduct = 1,
+
+        /// <summary>
+        /// To edit product
+        /// </summary>
+        EditProduct,
+
+        /// <summary>
+        /// To delete product
+        /// </summary>
+        DeleteProduct,
+
+        /// <summary>
+        /// To search product
+        /// </summary>
+        SearchProduct,
+
+        /// <summary>
+        /// To display products
+        /// </summary>
+        DisplayProduct,
+
+        /// <summary>
+        /// To exit the menu.
+        /// </summary>
+        Exit,
+    }
+}
