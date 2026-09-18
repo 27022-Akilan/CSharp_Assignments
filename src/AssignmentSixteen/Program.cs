1e611f6f17c860b3c88ff0b76ef9b3d8fbb009ea
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-09-18 14:55:44 +0530

    build : task 1

---
+++ src/AssignmentSixteen/Program.cs
@@ -0,0 +1,30 @@
+﻿using AssignmentSixteen;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Represents the entry point of the application.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Starts the application.
+        /// </summary>
+        /// <param name="args">Default arguments.</param>
+        public static void Main(string[] args)
+        {
+            Notifier notifier = new Notifier();
+            notifier.OnAction += DisplayMessage;
+            notifier.SendNotification();
+        }
+
+        /// <summary>
+        /// Displays the message to the user.
+        /// </summary>
+        /// <param name="message">Message to be displayed.</param>
+        public static void DisplayMessage(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
