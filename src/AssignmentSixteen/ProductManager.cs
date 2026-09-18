7e94cc1f4527d17c4b5c6cdfd021250482a6d827
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-09-18 16:35:08 +0530

    build : task 5

---
+++ src/AssignmentSixteen/ProductManager.cs
@@ -0,0 +1,52 @@
+﻿namespace AssignmentSixteen
+{
+    public class ProductManager
+    {
+        /// <summary>
+        /// Sorting delegate that takes two products ans returns an int representing the result.
+        /// </summary>
+        /// <param name="product1">Product 1</param>
+        /// <param name="product2">Product 2</param>
+        /// <returns>-1 if product 1 parameter is smaller than product 2 parameter
+        /// 0 If two products parameters are equal
+        /// 1 otherwise</returns>
+        public delegate int SortDelegate(Product product1, Product product2);
+
+        /// <summary>
+        /// Creates a list of products.
+        /// </summary>
+        /// <returns>List of products.</returns>
+        public List<Product> CreateProducts()
+        {
+            List<Product> products = new List<Product>()
+            {
+                new Product("Laptop", "Electronics", 1200.00m),
+                new Product("Smartphone", "Electronics", 800.00m),
+                new Product("Tablet", "Electronics", 500.00m),
+                new Product("Headphones", "Accessories", 150.00m),
+                new Product("Keyboard", "Accessories", 100.00m),
+            };
+
+            return products;
+        }
+
+        /// <summary>
+        /// Sorts the products based on the comparator function (sortDelegate) and displays the products in sorted order.
+        /// </summary>
+        /// <param name="products">List of products to be sorted</param>
+        /// <param name="sortDelegate">Delegate representing the particular compare parameter.</param>
+        public void SortAndDisplayProducts(List<Product> products, SortDelegate sortDelegate)
+        {
+            products.Sort((product1, product2) => sortDelegate(product1, product2));
+            this.DisplayProducts(products);
+        }
+
+        private void DisplayProducts(List<Product> products)
+        {
+            foreach (Product product in products)
+            {
+                Console.WriteLine($"Name : {product.Name} \t Category : {product.Category} \t Price : {product.Price}");
+            }
+        }
+    }
+}
--- src/AssignmentSixteen/Program.cs
+++ src/AssignmentSixteen/Program.cs
@@ -22,6 +22,12 @@ namespace Assignments
 
             ListOperator listOperator = new ListOperator();
             listOperator.FilterAndSquareEvenNumbers(new List<int> { 1, 2, 3, 4, 5, 6 });
+
+            ProductManager productManager = new ProductManager();
+            List<Product> products = productManager.CreateProducts();
+            productManager.SortAndDisplayProducts(products, SortByName);
+            productManager.SortAndDisplayProducts(products, SortByCategory);
+            productManager.SortAndDisplayProducts(products, SortByPrice);
         }
 
         /// <summary>
@@ -31,6 +37,51 @@ namespace Assignments
         public static void DisplayMessage(string message)
         {
             Console.WriteLine(message);
+        }
+
+        /// <summary>
+        /// Compares product by name and returns an integer representing the result.
+        /// </summary>
+        /// <param name="product1">Product 1</param>
+        /// <param name="product2">Product 2</param>
+        /// <returns>
+        /// -1 if product 1 name is smaller than product 2 name
+        /// 0 If two products names are equal
+        /// 1 otherwise
+        /// </returns>
+        public static int SortByName(Product product1, Product product2)
+        {
+            return string.Compare(product1.Name, product2.Name);
+        }
+
+        /// <summary>
+        /// Compares product by category and returns an integer representing the result.
+        /// </summary>
+        /// <param name="product1">Product 1</param>
+        /// <param name="product2">Product 2</param>
+        /// <returns>
+        /// -1 if product 1 category is smaller than product 2 category
+        /// 0 If two products category are equal
+        /// 1 otherwise
+        /// </returns>
+        public static int SortByCategory(Product product1, Product product2)
+        {
+            return string.Compare(product1.Category, product2.Category);
+        }
+
+        /// <summary>
+        /// Compares product by price and returns an integer representing the result.
+        /// </summary>
+        /// <param name="product1">Product 1</param>
+        /// <param name="product2">Product 2</param>
+        /// <returns>
+        /// -1 if product 1 price is smaller than product 2 price
+        /// 0 If two products price are equal
+        /// 1 otherwise
+        /// </returns>
+        public static int SortByPrice(Product product1, Product product2)
+        {
+            return product1.Price.CompareTo(product2.Price);
         }
     }
 }
