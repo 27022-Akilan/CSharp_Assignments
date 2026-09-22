4c65c8d125726c8c216148a55e04a091c09d7fb1
27022-Akilan <akilan.ponnusamy@solitontech.com>
2026-09-22 11:24:32 +0530

    chore : merge assignment 11 into main
    * build : folder
    
    * build : task 1
    
    * build : task 2
    
    * build : task 4
    
    * fix : parameter change in GetTotMem method task 2
    
    * fix : seperate read and write files
    
    * build : add md file
    
    * fix : delete unwanted files
    
    * fix : pr-comment add content about stack,heap,how GC runs in the readme
    
    * fix : indendation on Readme
    
    * fix : add Docs and put readme into it
    
    * fix : rename folder name Docs -> Documents
    
    * fix : demo documenation fix
    
    * fix : remove temp text files
    
    * fix : create a seperate for asg-11 folder inside documents and place md into it
    
    * fix : delete temp file
    
    * fix : delete extra folder

---
+++ src/GarbageCollector/Model/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace GarbageCollector.Model
+{
+    /// <summary>
+    /// Represents the person information.
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Gets the data of the person (just a byte for seeing the memory allocation.)
+        /// </summary>
+        /// <value>Data of the person</value>
+        public byte[] Data => new byte[1024];
+    }
+}
---
+++ src/IDisposableDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using IDisposableDemo;
+
+namespace Assignments
+{
+    /// <summary>
+    /// Starting point of the application.
+    /// </summary>
+    public class Program
+    {
+        /// <summary>
+        /// Starts the application.
+        /// </summary>
+        /// <param name="args">Default arguments.</param>
+        public static void Main(string[] args)
+        {
+            string filePath = "Demo.txt";
+            using (var fileWriter = new FileWriter(filePath))
+            {
+                fileWriter.Write("Hii buddy, Happy coding");
+            }
+
+            // Test whether the file can be opened again as the previous one releases the handle.
+            using (var fileReader = new FileReader(filePath))
+            {
+                fileReader.Read();
+            }
+        }
+    }
+}
